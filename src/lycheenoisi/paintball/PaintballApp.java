17d2a2446afa9558156de224274df51e63162f47
Severine Schiltz <schiltz.severine@gmail.com>
2020-10-16 18:58:07 +0200

    feat: ajout d'un test pour vérifier que le projet compile bien

--- src/lycheenoisi/paintball/PaintballApp.java
+++ src/lycheenoisi/paintball/PaintballApp.java
@@ -5,5 +5,7 @@ import lycheenoisi.paintball.model.Member;
 import lycheenoisi.paintball.view.View;
 
 public class PaintballApp {
-
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }

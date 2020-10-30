1a652ebfcaa89461df8af0792cccab729da5c2ba
Ines Boudghene <ines.boudghene@gmail.com>
2020-10-30 01:19:18 +0100

    fix(PaintballApp): Start menu access
    replace LoginController by StartMenuController and clear comments

--- src/lycheenoisi/paintball/PaintballApp.java
+++ src/lycheenoisi/paintball/PaintballApp.java
@@ -1,8 +1,6 @@
 package lycheenoisi.paintball;
-import lycheenoisi.paintball.controller.DisplayReservationsController;
-import lycheenoisi.paintball.controller.LoginController;
+import lycheenoisi.paintball.controller.StartMenuController;
 import lycheenoisi.paintball.model.Model;
-import lycheenoisi.paintball.model.Member;
 import lycheenoisi.paintball.model.User;
 import lycheenoisi.paintball.view.ErrorView;
 
@@ -17,14 +15,11 @@ public class PaintballApp {
         return loggedUser;
     }
 
-    //test UC_DisplayReservations
     public static void main(String[] args) {
-        //new MemberListController().run();
         if (!Model.checkDb())
             new ErrorView("Database is not available").close();
         else
-            //new MemberListController().run();
-            new LoginController().run();
+            new StartMenuController().run();
     }
 
 //    //public static void main(String[] args) {

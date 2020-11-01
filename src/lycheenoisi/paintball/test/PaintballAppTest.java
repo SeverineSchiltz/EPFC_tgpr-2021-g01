2a29244cd44c377783faaa08f4995cf63b53c348
Severine Schiltz <schiltz.severine@gmail.com>
2020-11-01 13:35:09 +0100

    refactor(UC_DisplayReservation) : suppresion de ce uc qui est remplacé par UC_CancelReservation

--- src/lycheenoisi/paintball/test/PaintballAppTest.java
+++ src/lycheenoisi/paintball/test/PaintballAppTest.java
@@ -1,7 +1,6 @@
 package lycheenoisi.paintball.test;
 
 import lycheenoisi.paintball.controller.CancelReservationController;
-import lycheenoisi.paintball.controller.DisplayReservationsController;
 import lycheenoisi.paintball.model.Member;
 import lycheenoisi.paintball.model.Model;
 import lycheenoisi.paintball.view.ErrorView;
@@ -11,16 +10,7 @@ import static lycheenoisi.paintball.PaintballApp.setLoggedUser;
 import static org.junit.Assert.*;
 
 public class PaintballAppTest {
-
-//    @Test //plus utilisé:
-//    public void test_UC_DisplayReservations(){
-//        if (!Model.checkDb()) {
-//            new ErrorView("Database is not available").close();
-//        }else {
-//            setLoggedUser(new Member("sschilt"));
-//            new DisplayReservationsController().run();
-//        }
-//    }
+    
 
     @Test
     public void test_UC_CancelReservations(){

046226a05ce1516b9214c1faf5911c5a518a0373
Nikola <vorkapicnikola00@gmail.com>
2020-11-01 12:40:21 +0100

    feat(UC_CancelReservation): Merge conflict

--- src/lycheenoisi/paintball/PaintballApp.java
+++ src/lycheenoisi/paintball/PaintballApp.java
@@ -2,6 +2,7 @@ package lycheenoisi.paintball;
 import lycheenoisi.paintball.controller.CancelReservationController;
 import lycheenoisi.paintball.controller.DisplayReservationsController;
 import lycheenoisi.paintball.controller.LoginController;
+import lycheenoisi.paintball.controller.StartMenuController;
 import lycheenoisi.paintball.model.Model;
 import lycheenoisi.paintball.model.Member;
 import lycheenoisi.paintball.view.ErrorView;
@@ -19,18 +20,14 @@ public class PaintballApp {
 
     //test UC_DisplayReservations
     public static void main(String[] args) {
-        //new MemberListController().run();
-        //setLoggedUser(new Member("nvorkap"));
-        if (!Model.checkDb())
-            new ErrorView("Database is not available").close();
-        else
-            //new MemberListController().run();
-            new LoginController().run();
-            //new CancelReservationController().run();
-    }
 
-//    //public static void main(String[] args) {
-//        System.out.println("Ca compile! :-)");
-//    }
+        if (!Model.checkDb())
+
+            new ErrorView("Database is not available").close();
+
+        else
+
+            new StartMenuController().run();
+    }
 
 }

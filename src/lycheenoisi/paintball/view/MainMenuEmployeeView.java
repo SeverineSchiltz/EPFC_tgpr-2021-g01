2c304aac8823e7cb5c97fee96e93d04a3422ac17
Severine Schiltz <schiltz.severine@gmail.com>
2020-11-01 17:52:01 +0100

    fix(UC_MainMenuEmployee): correction des méthodes doAskForAction

--- src/lycheenoisi/paintball/view/MainMenuEmployeeView.java
+++ src/lycheenoisi/paintball/view/MainMenuEmployeeView.java
@@ -7,12 +7,12 @@ public class MainMenuEmployeeView extends View {
 
     public View.Action askForAction(int size) {
         return doAskForAction(size, "\n[M] Display members, [R] Display all reservation, [L] Logout",
-                "[vV]+|[fF]+|[lL]");
+                "[mM]+|[rR]+|[lL]");
     }
 
     public View.Action askForActionAdmin(int size) {
         return doAskForAction(size, "\n[M] Display members, [R] Display all reservation,[E] Display employees, [L] Logout",
-                "[vV]+|[fF]+|[eE]+|[lL]");
+                "[mM]+|[rR]+|[eE]+|[lL]");
     }
 
 }

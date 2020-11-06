28ff6cca4adc150383f8e0f04559efef6cdcff33
Leyla Malsagova <leyla.malsagova@gmail.com>
2020-11-06 08:32:15 +0100

    fix(database) DB access

--- src/lycheenoisi/paintball/model/Model.java
+++ src/lycheenoisi/paintball/model/Model.java
@@ -9,7 +9,7 @@ public abstract class Model {
 
     static {
         try {
-            db = DriverManager.getConnection("jdbc:mariadb://localhost:8080/paintball?user=root&password=");
+            db = DriverManager.getConnection("jdbc:mariadb://localhost:3306/paintball?user=root&password=root");
         } catch (SQLException e) {
             e.printStackTrace();
         }

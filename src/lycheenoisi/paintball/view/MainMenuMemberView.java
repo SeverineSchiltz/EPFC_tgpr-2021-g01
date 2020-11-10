2aa213e25d0d4be3da3a41d59dcf52cd74e4e731
Leyla Malsagova <leyla.malsagova@gmail.com>
2020-11-11 00:35:36 +0100

    feat(UC_MainMenuMember) + Display field option

--- src/lycheenoisi/paintball/view/MainMenuMemberView.java
+++ src/lycheenoisi/paintball/view/MainMenuMemberView.java
@@ -1,19 +1,17 @@
 package lycheenoisi.paintball.view;
 
-import lycheenoisi.paintball.PaintballApp;
-
 public class MainMenuMemberView extends View {
     public void displayHeader() {
         displayHeader("Main menu member:");
     }
 
     public View.Action askForAction(int size) {
-        return doAskForAction(size, "\n[R] Make a reservation, [E] Edit profile, [A] Display reservations, [L] Logout",
-                "[rR]+|[eE]+|[aA]+|[lL]");
+        return doAskForAction(size, "\n[R] Make a reservation, [E] Edit profile, [F] Display Fields, [A] Display reservations, [L] Logout",
+                "[rR]+|[eE]+|[fF]+|[aA]+|[lL]");
     }
 
     public View.Action askForActionVIP(int size) {
-        return doAskForAction(size, "\n[R] Make a reservation, [E] Edit profile, [A] Display reservations, [L] Logout",
-                "[rR]+|[eE]+|[aA]+|[lL]");
+        return doAskForAction(size, "\n[R] Make a reservation, [E] Edit profile, [F] Display Fields, [A] Display reservations, [L] Logout",
+                "[rR]+|[eE]+|[fF]+|[aA]+|[lL]");
     }
 }

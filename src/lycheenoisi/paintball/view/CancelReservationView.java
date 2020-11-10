8921a1244b725b1cd8a7614efa6228461bd845b9
Sin <sinou.94@hotmail.fr>
2020-11-11 00:22:48 +0100

    refactor: translated CancelReservationView

--- src/lycheenoisi/paintball/view/CancelReservationView.java
+++ src/lycheenoisi/paintball/view/CancelReservationView.java
@@ -3,7 +3,7 @@ package lycheenoisi.paintball.view;
 public class CancelReservationView extends View{
 
     public int askCancelReservationNumber() {
-        String answer = askString("Numéro de réservation: ", null);
+        String answer = askString("Reservation number: ", null);
         return Integer.parseInt(answer);
     }
 

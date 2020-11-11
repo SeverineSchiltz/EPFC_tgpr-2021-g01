1429c9aa911a9cb04560906fc58dc9318cf0c346
Leyla Malsagova <leyla.malsagova@gmail.com>
2020-11-11 21:51:47 +0100

    fix(UC_DisplayAvailableFields) + fightType name validation

--- src/lycheenoisi/paintball/controller/DisplayAvailableFieldsController.java
+++ src/lycheenoisi/paintball/controller/DisplayAvailableFieldsController.java
@@ -1,5 +1,6 @@
 package lycheenoisi.paintball.controller;
 import lycheenoisi.paintball.model.Field;
+import lycheenoisi.paintball.model.FightType;
 import lycheenoisi.paintball.model.Timeslot;
 import lycheenoisi.paintball.view.DisplayAvailableFieldsView;
 import lycheenoisi.paintball.view.View;
@@ -17,17 +18,25 @@ public class DisplayAvailableFieldsController extends Controller {
             do {
                 view.displayHeader();
                 LocalDate date = view.askDate();
+
                 String inputTimeslot=null;
+                Timeslot timeslot=null;
+                boolean valid = false;
                 do {
                     inputTimeslot = view.askTimeslot();
-                    inputTimeslot = inputTimeslot.substring(0, 1).toUpperCase() + inputTimeslot.substring(1, inputTimeslot.length()).toLowerCase();
+                    if(inputTimeslot!=null){ inputTimeslot = inputTimeslot.substring(0, 1).toUpperCase() + inputTimeslot.substring(1, inputTimeslot.length()).toLowerCase();}
+                    for(Timeslot t : Timeslot.values()){
+                        if(inputTimeslot!=null && inputTimeslot.equals(t.name())){
+                            valid = true;
+                        }
+                    }
+                }while(!valid);
+                timeslot=Timeslot.valueOf(inputTimeslot);
+                String fightType=null;
+                do {
+                    fightType = view.askFightType();
+                }while(!FightType.isValidFightTypeName(fightType));
 
-                }while(!inputTimeslot.equals("Morning") && !inputTimeslot.equals("Evening") && !inputTimeslot.equals("Afternoon"));
-                Timeslot timeslot=null;
-                if(inputTimeslot!=null){
-                    timeslot=Timeslot.valueOf(inputTimeslot);
-                }
-                String fightType = view.askFightType();
                 var fields = Field.getAvailableFields(date, timeslot ,fightType);
                 view.displayAvailableFields(fields);
                 res = view.askForAction(fields.size());
--- src/lycheenoisi/paintball/model/FightType.java
+++ src/lycheenoisi/paintball/model/FightType.java
@@ -1,5 +1,8 @@
 package lycheenoisi.paintball.model;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class FightType extends Model {
     private String name;
     private String description;
@@ -38,5 +41,30 @@ public class FightType extends Model {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public static boolean isValidFightTypeName(String nameToValidate){
+        String request=null;
+        request = "SELECT ft.name 'ft_name' from  fight_type ft";
+        ArrayList<String> ftNames = new ArrayList<String>();
+
+        try {
+            var stmt = db.prepareStatement(request);
+            var rs = stmt.executeQuery();
+            while (rs.next()) {
+                String ftName = rs.getString("ft_name");
+                ftNames.add(ftName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ftn input : "+nameToValidate);
+        for(String ftn : ftNames){
+            System.out.println("ftn db : "+ftn);
+            if(nameToValidate!=null && nameToValidate.toUpperCase().equals(ftn)){
+                return true;
+            }
+        }
+        return false;
     }
 }

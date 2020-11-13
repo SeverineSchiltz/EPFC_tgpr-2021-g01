6b951fb8ad9374d07b8df5119e3ac9d07bb0f85f
Sinouhe <sinou.94@hotmail.fr>
2020-11-14 00:16:48 +0100

    fix(UC_MainMenuMember): re-added display available fields in the view

--- src/lycheenoisi/paintball/view/MainMenuMemberView.java
+++ src/lycheenoisi/paintball/view/MainMenuMemberView.java
@@ -6,12 +6,12 @@ public class MainMenuMemberView extends View {
     }
 
     public View.Action askForAction(int size) {
-        return doAskForAction(size, "\n[B] Make a reservation\n[E] Edit profile\n[R] Display reservations\n[A] Display fields\n[F] Display fight types\n[Q] Display equipments\n[Q] Display available fields\n[L] Logout",
+        return doAskForAction(size, "\n[B] Make a reservation\n[E] Edit profile\n[R] Display reservations\n[A] Display fields\n[F] Display fight types\n[Q] Display equipments\n[D] Display available fields\n[L] Logout",
                 "[bB]+|[eE]+|[rR]+|[aA]+|[fF]+|[qQ]+|[lL]");
     }
 
     public View.Action askForActionVIP(int size) {
-        return doAskForAction(size, "\n[B] Make a reservation\n[E] Edit profile\n[R] Display reservations\n[A] Display fields\n[F] Display fight types\n[Q] Display equipments\n[Q] Display available fields\n[L] Logout",
+        return doAskForAction(size, "\n[B] Make a reservation\n[E] Edit profile\n[R] Display reservations\n[A] Display fields\n[F] Display fight types\n[Q] Display equipments\n[D] Display available fields\n[L] Logout",
                 "[bB]+|[eE]+|[rR]+|[aA]+|[fF]+|[qQ]+|[lL]");
     }
 }

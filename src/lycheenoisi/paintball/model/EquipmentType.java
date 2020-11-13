1ee294483eaec3c6f009c4ab584e54f756ad5966
Severine Schiltz <schiltz.severine@gmail.com>
2020-11-13 15:06:47 +0100

    fix(UC_DisplayReservation) : suppression d'une partie de code non nécessaire qui créait des bug dans l'appli

--- src/lycheenoisi/paintball/model/EquipmentType.java
+++ src/lycheenoisi/paintball/model/EquipmentType.java
@@ -8,7 +8,7 @@ public class EquipmentType extends Model{
     private double rent_price;
     private double sell_price;
 
-    public EquipmentType(String name, double rent_price, double sell_price){
+    public EquipmentType(String name, double rent_price){
         this.setName(name);
         this.setRent_price(rent_price);
         this.setRent_price(sell_price);

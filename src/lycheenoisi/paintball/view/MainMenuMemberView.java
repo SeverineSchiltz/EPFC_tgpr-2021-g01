e0e4bb6d81d8d74f4e7c379a8cfb5778f78648a1
Sinouhe <sinou.94@hotmail.fr>
2020-11-14 00:18:10 +0100

    fix(UC_MainMenuMember): re-added display available fields in the view

--- src/lycheenoisi/paintball/view/MainMenuMemberView.java
+++ src/lycheenoisi/paintball/view/MainMenuMemberView.java
@@ -7,11 +7,11 @@ public class MainMenuMemberView extends View {
 
     public View.Action askForAction(int size) {
         return doAskForAction(size, "\n[B] Make a reservation\n[E] Edit profile\n[R] Display reservations\n[A] Display fields\n[F] Display fight types\n[Q] Display equipments\n[D] Display available fields\n[L] Logout",
-                "[bB]+|[eE]+|[rR]+|[aA]+|[fF]+|[qQ]+|[lL]");
+                "[bB]+|[eE]+|[rR]+|[aA]+|[fF]+|[qQ]+|[lL]+|[dD]");
     }
 
     public View.Action askForActionVIP(int size) {
         return doAskForAction(size, "\n[B] Make a reservation\n[E] Edit profile\n[R] Display reservations\n[A] Display fields\n[F] Display fight types\n[Q] Display equipments\n[D] Display available fields\n[L] Logout",
-                "[bB]+|[eE]+|[rR]+|[aA]+|[fF]+|[qQ]+|[lL]");
+                "[bB]+|[eE]+|[rR]+|[aA]+|[fF]+|[qQ]+|[lL]+|[dD]");
     }
 }
